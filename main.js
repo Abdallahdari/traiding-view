cadf5e68af3972f8e2beb0ca0b6a4eb3367ab2e1
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2024-06-22 01:34:49 +0300

    added the live account chart part

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log(`bismiillah`) ;
 const nabar=document.querySelector('.navbar')
-console.log(nabar) ;
+// console.log(nabar) ;
 nabar.classList.remove('fixed','shadow-md')
 const section1=document.querySelector('.section--1')
 console.log(section1) ;
@@ -23,14 +23,22 @@ else{
 }
 const sect=new IntersectionObserver(sticky,{
     root:null,
-    threshold:0.6,
-    rootMargin:`20px`,
+    threshold:0.2,
+    rootMargin:`-100px`,
 })
 sect.observe(section1)
 const list=document.querySelector('.listi')
 const drop=document.querySelectorAll('.dropdown')
-console.log(list) ;
-list.addEventListener('mouseover',function(e){
-    const click=e.target
-    console.log(click) ;
+// console.log(list) ;
+// list.addEventListener('mouseover',function(e){
+//     const click=e.target
+//     console.log(click) ;
+// })
+const links=document.querySelector('.likss')
+const lkkl=document.querySelector('.linkrs')
+const dropd=document.querySelector('.dor')
+// console.log(lkkl.childNodes) ;
+nabar.addEventListener('mouseover',function(e){
+    const tiger=e.target.closest('.likss')
+  console.log(tiger) ;  
 })
